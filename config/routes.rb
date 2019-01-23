45d255898da301188428d65af767f1d40a3728ed
Ezgi Seyis <ezgicay_1993@hotmail.com>
2019-01-23 16:15:02 +0100

    2 Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:index, :show, :new, :create]
+  end
+  resources :reviews, only: [:new, :create]
+
+  namespace :admin do
+    resources :restaurants, only: [:index, :update, :delete]
+  end
+
+  root to: 'restaurants#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Vapiano',
+    address:      'Ingolstadt',
+    phone_number: '+49 87493743',
+    category:     'italian'
+
+  },
+  {
+    name:         'Asia Palast',
+    address:      'München',
+    phone_number: '+49 112345',
+    category:     'chinese'
+  },
+   {
+    name:         'Cafe de Paris',
+    address:      'Berlin',
+    phone_number: '0873947 7434324',
+    category:     'french'
+  },
+   {
+    name:         'Whok Whok',
+    address:      'München',
+    phone_number: '0596-785775',
+    category:     'japanese'
+  },
+   {
+    name:         'Test Test',
+    address:      'Köln',
+    phone_number: '452345345',
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
